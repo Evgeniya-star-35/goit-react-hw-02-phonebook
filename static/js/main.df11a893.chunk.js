fa9f88e4dcf8b42fb5d55638d8a6459805c4ada8
Evgeniya Bayda <baje75128@gmail.com>
2021-10-06 22:40:56 +0300

    Updates

--- static/js/main.df11a893.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),a=(c(8),c(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("header",{className:"App-header"})})};s.a.render(Object(a.jsx)(o.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.df11a893.chunk.js.map
